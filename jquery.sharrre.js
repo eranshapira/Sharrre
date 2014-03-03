eebdae73d7b410cb786fbf00beee6e3df77d81c4
lxeran <lxeran@gmail.com>
2014-03-03 15:50:25 +0200

    Update jquery.sharrre.js
    Facebook maintains a limit for requests, while on low-traffic sites this means nothing,
    on high-level sites, this might pose as a critical issue.
    
    The solution here has two levels:
    1. use facebook cache on akamai
    2. use server-side controller (metadatacontroller.cs) for c# based sites - which enables us to use server-side caching, and save redundant calls to facebook.
    
    of course the result means that there is a delay between each update on facebook shares (rough estimate - 30 minutes delay).
    
    I think this is completely tolerable although tests might fail because of this.

--- jquery.sharrre.js
+++ jquery.sharrre.js
@@ -101,9 +101,9 @@
     googlePlus: "",
 
 	//new FQL method by Sire
-	facebook: "https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27{url}%27&callback=?",
+	//facebook: "https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27{url}%27&callback=?",
     //old method facebook: "http://graph.facebook.com/?id={url}&callback=?",
-    //facebook : "http://api.ak.facebook.com/restserver.php?v=1.0&method=links.getStats&urls={url}&format=json"
+    facebook : "http://api.ak.facebook.com/restserver.php?v=1.0&method=links.getStats&urls={url}&format=json",
     
     twitter: "http://cdn.api.twitter.com/1/urls/count.json?url={url}&callback=?",
     digg: "http://services.digg.com/2.0/story.getInfo?links={url}&type=javascript&callback=?",
@@ -365,6 +365,7 @@
     if(this.options.urlCurl !== ''){
       urlJson.googlePlus = this.options.urlCurl + '?url={url}&type=googlePlus'; // PHP script for GooglePlus...
       urlJson.stumbleupon = this.options.urlCurl + '?url={url}&type=stumbleupon'; // PHP script for Stumbleupon...
+      urlJson.facebook = this.options.urlCurl + '?url={url}&type=facebook'; // PHP script for Facebook...
     }
     $(this.element).addClass(this.options.className); //add class
     
